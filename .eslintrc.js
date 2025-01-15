fd5f5cca7199e1dea58a350ef5f22118246d908f
Dmitry-Loburets <dima-loburec@yandex.ru>
2025-01-15 18:14:17 +0600

    Add minification and linting

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'no-unused-vars': 'warn',
+        'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+    },
+};
